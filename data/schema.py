7ace3047cc829ec74b0e9697c2140be2387a1fdf
SARAB0297 <297sarab.work@gmail.com>
2026-01-15 04:30:44 +0530

    Initial commit

---
+++ data/schema.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+HOLDINGS_REQUIRED_COLUMNS: List[str] = [
+    "PortfolioName",
+    "SecName",
+    "Qty",
+    "PL_DTD",
+    "PL_MTD",
+    "PL_QTD",
+    "PL_YTD"
+]
+
+
+
+TRADES_REQUIRED_COLUMNS: List[str] = [
+    "PortfolioName",
+    "TradeTypeName",
+    "AsOfDate",
+    "PL_DTD",
+    "PL_MTD",
+    "PL_QTD",
+    "PL_YTD"
+]
